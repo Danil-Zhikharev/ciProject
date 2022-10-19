a4529be9a2a2f8710a80e54a96075b0b81d81fa5
zda <zda@ap-soft.ru>
2022-10-19 17:19:37 +0500

    Commit_17:19_19.10

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,11 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMax_1() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+        System.out.println("Ожидаемое: " + expected);
+        System.out.println("Фактическое: " + actual);
+    }
+
+    @Test
+    void findMax_2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, -1, 3, -3, -4, -5, -6, -7, -8, -9, -10};
+        long expected = 3;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
